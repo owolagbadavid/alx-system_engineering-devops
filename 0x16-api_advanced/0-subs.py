e4d8ef702f5dab4d053f8fe3279cdbcff5255de8
DAVID <owolagbadavid@gmail.com>
2024-02-14 22:56:48 +0100

    fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import requests
 
 
@@ -14,7 +15,7 @@ def number_of_subscribers(subreddit):
     """
 
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "linux:0x16.api.advanced:v1.0(by /u/oreosinit)"}
+    headers = {"User-Agent": "linux:0x16.api.advanced:v1.0 (by /u/oreosinit)"}
 
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
